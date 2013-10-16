3ed2019ef8f446eb7df6c892c41243df06ad115e
Trever <trever@iron-point.com>
2013-10-15 21:16:03 -0700

    EOD
    Created GitHub Repo, workflow directory, setup CodeKit for Sass files,
    and got @font-face support in place.

---
+++ config.rb
@@ -0,0 +1,23 @@
+# Require any additional compass plugins here.
+require 'susy'
+
+# Set this to the root of your project when deployed:
+css_dir         = "css"
+fonts_dir       = "fonts"
+http_path       = "html"
+images_dir      = "images"
+javascripts_dir = "js"
+sass_dir        = "sass"
+
+project_type    = :stand_alone
+
+encoding        = 'utf-8'
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded #:expanded or :nested or :compact or :nested or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
